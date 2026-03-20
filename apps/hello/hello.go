59c8bf93ce669f485910eddc1a025e39af0681a1
ndinhbang <ndinhbang111@gmail.com>
2026-03-20 11:25:37 +0700

    Return and handle an error

--- apps/hello/hello.go
+++ apps/hello/hello.go
@@ -2,12 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/ndinhbang/learn-go/pkg/greetings"
 )
 
 func main() {
 	// Get a greeting message and print it.
-	message := greetings.Hello("Bang")
+	message, err := greetings.Hello("")
+
+	// If an error was returned, print it to the console and
+	// exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error was returned, print the returned message
+	// to the console.
 	fmt.Println(message)
 }
--- pkg/greetings/greetings.go
+++ pkg/greetings/greetings.go
@@ -1,15 +1,20 @@
 package greetings
 
 import (
+	"errors"
 	"fmt"
 )
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message
+	if name == "" {
+		return "", errors.New("empty name")
+	}
 	// Return a greeting that embeds the name in a message.
 	// var message string = fmt.Sprintf("Hi, %v. Welcome!", name)
 	// var message string
 	// message = fmt.Sprintf("Hi, %v. Welcome!", name)
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
