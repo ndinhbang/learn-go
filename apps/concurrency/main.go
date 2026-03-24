78be763dea7aec5507ef466837601f1b2779f773
ndinhbang <ndinhbang111@gmail.com>
2026-03-24 14:20:13 +0700

    separate concurrency logic from business logic

--- apps/concurrency/main.go
+++ apps/concurrency/main.go
@@ -6,8 +6,8 @@ import (
 	"time"
 )
 
-func infiniteCount(thing string, wg *sync.WaitGroup) {
-	defer wg.Done() // Indicates that this goroutine is done when the function returns.
+func infiniteCount(thing string) {
+
 	for i := 1; i <= 5; i++ {
 		fmt.Println(i, thing)
 		time.Sleep(time.Second * 1)
@@ -19,8 +19,15 @@ func main() {
 	var wg sync.WaitGroup
 	fmt.Println("Starting goroutines...")
 	wg.Add(2) // Add the number of goroutines we are going to wait for (2 in this case).
-	go infiniteCount("dog", &wg)
-	go infiniteCount("cat", &wg)
+	go func() {
+		defer wg.Done() // Indicates that this goroutine is done when the function returns.
+		infiniteCount("dog")
+	}()
+
+	go func() {
+		defer wg.Done() // Indicates that this goroutine is done when the function returns.
+		infiniteCount("cat")
+	}()
 	// fmt.Println("Waiting for goroutines to finish...")
 	wg.Wait() // Wait for all goroutines to finish before proceeding to the next line of code.
 	fmt.Println("All goroutines finished.")
