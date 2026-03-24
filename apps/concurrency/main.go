1e2297b50b1281476883fd22b972153c3f1463f5
ndinhbang <ndinhbang111@gmail.com>
2026-03-24 16:47:17 +0700

    close channel to prevent dead locked

--- apps/concurrency/main.go
+++ apps/concurrency/main.go
@@ -15,13 +15,23 @@ func main() {
 		for _, word := range words {
 			in <- word
 		}
+		// close the channel to signal that there are no more words
+		// only one goroutine should close the channel
+		// if multiple goroutines close the channel, it will panic
+		// so we need to make sure that only one goroutine closes the channel
+		// and the other goroutines should not close the channel
+		close(in)
 	}()
 
 	for { // (2)
-		word := <-in
-		if word != "" {
-			fmt.Printf("%s ", word)
+		// receive from the channel
+		word, ok := <-in
+		// if the channel is closed, break the loop
+		// if the channel is closed, the ok will be false
+		// if the channel is not closed, the ok will be true
+		if !ok {
+			break
 		}
+		fmt.Printf("%s ", word)
 	}
-	// one two four
 }
