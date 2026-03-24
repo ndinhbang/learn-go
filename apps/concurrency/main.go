5a69c551ee5dfd19715637af6c1d1c34bbac1ab0
ndinhbang <ndinhbang111@gmail.com>
2026-03-24 14:55:06 +0700

    feat(concurrency): Implement digit counting in words using goroutines

--- apps/concurrency/main.go
+++ apps/concurrency/main.go
@@ -2,32 +2,73 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"sync"
-	"time"
+	"unicode"
 )
 
-func infiniteCount(thing string) {
+// Create a function countDigitsInWords that takes an input string,
+// splits it into words, and counts the digits in each word using countDigits.
+// Be sure to do the counting for each word in a separate goroutine.
 
-	for i := 1; i <= 5; i++ {
-		fmt.Println(i, thing)
-		time.Sleep(time.Second * 1)
+// countDigits returns the number of digits in a string.
+func countDigits(str string) int {
+	count := 0
+	for _, char := range str {
+		if unicode.IsDigit(char) {
+			count++
+		}
+	}
+	return count
+}
+
+// counter stores the number of digits in each word.
+// The key is the word, and the value is the number of digits.
+type counter map[string]int
+
+func countDigitsInWords(phrase string) counter {
+	var wg sync.WaitGroup
+	syncStats := new(sync.Map)
+	words := strings.FieldsSeq(phrase) // Split the phrase into words using FieldsSeq to handle multiple spaces.
+
+	for word := range words {
+		wg.Go(func() {
+			count := countDigits(word)
+			syncStats.Store(word, count)
+		})
+	}
+	wg.Wait()
+
+	return asStats(syncStats)
+}
+
+// asStats converts statistics from sync.Map to a regular map.
+func asStats(m *sync.Map) counter {
+	stats := counter{}
+	m.Range(func(word, count any) bool {
+		stats[word.(string)] = count.(int)
+		return true
+	})
+	return stats
+}
+
+// printStats prints the number of digits in words.
+func printStats(stats counter) {
+	for word, count := range stats {
+		fmt.Printf("%s: %d\n", word, count)
 	}
 }
 
 // Running both count functions as goroutines.
 func main() {
-	var wg sync.WaitGroup
-	fmt.Println("Starting goroutines...")
-	// The WaitGroup.Go method (Go 1.25+) is used to start a goroutine and automatically manage the WaitGroup counter.
-	// It increments the counter when the goroutine starts and decrements it when the goroutine finishes.
-	// This eliminates the need for manual calls to wg.Add(1) and defer wg.Done() in each goroutine.
-	wg.Go(func() {
-		infiniteCount("dog")
-	})
-	wg.Go(func() {
-		infiniteCount("cat")
-	})
-	// fmt.Println("Waiting for goroutines to finish...")
-	wg.Wait() // Wait for all goroutines to finish before proceeding to the next line of code.
-	fmt.Println("All goroutines finished.")
+	phrase := "0ne 1wo thr33   4068"
+	counts := countDigitsInWords(phrase)
+	printStats(counts)
+
+	total := 0
+	for _, count := range counts {
+		total += count
+	}
+	fmt.Printf("Total digits: %d\n", total)
+
 }
