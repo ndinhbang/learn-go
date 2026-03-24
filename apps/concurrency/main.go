9ac6338649b935e52073937d7b57975b01e62bc9
ndinhbang <ndinhbang111@gmail.com>
2026-03-24 15:39:57 +0700

    feat(concurrency): Update message handling in goroutines for clarity and functionality

--- apps/concurrency/main.go
+++ apps/concurrency/main.go
@@ -2,18 +2,26 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 // Running both count functions as goroutines.
 func main() {
-	msgChannel := make(chan string)
+	messages := make(chan string)
 
 	go func() {
-		msgChannel <- "Hello from the goroutine!"
+		fmt.Println("B: Sending message...")
+		messages <- "ping"              // (1) After sending the message to the channel, goroutine B gets blocked
+		fmt.Println("B: Message sent!") // (2) This line will not be executed until goroutine A receives the message from the channel
 	}()
-	fmt.Println("Waiting for message from goroutine...")
-	msg := <-msgChannel // blocks until a message is received from the channel
-	fmt.Println("Received message from goroutine:")
-	fmt.Println(msg)
+
+	fmt.Println("A: Doing some work...")
+	time.Sleep(500 * time.Millisecond)
+	fmt.Println("A: Ready to receive a message...")
+
+	<-messages //  (3) Goroutine A receives the message from the channel, unblocking goroutine B and allowing it to continue its execution. After receiving the message, goroutine A can proceed with its work.
+
+	fmt.Println("A: Messege received!")
+	time.Sleep(100 * time.Millisecond)
 
 }
