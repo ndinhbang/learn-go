ec09a41104096c4d67670ed3ef3b037a5079ac3b
ndinhbang <ndinhbang111@gmail.com>
2026-03-24 16:43:01 +0700

    blocked channel

--- apps/concurrency/main.go
+++ apps/concurrency/main.go
@@ -3,66 +3,7 @@ package main
 import (
 	"fmt"
 	"strings"
-	"sync"
-	"unicode"
 )
-
-// Create a function countDigitsInWords that takes an input string,
-// splits it into words, and counts the digits in each word using countDigits.
-// Be sure to do the counting for each word in a separate goroutine.
-
-// countDigits returns the number of digits in a string.
-func countDigits(str string) int {
-	count := 0
-	for _, char := range str {
-		if unicode.IsDigit(char) {
-			count++
-		}
-	}
-	return count
-}
-
-// counter stores the number of digits in each word.
-// The key is the word, and the value is the number of digits.
-type counter map[string]int
-
-func countDigitsInWords(phrase string) counter {
-	countChan := make(chan int)
-	words := strings.FieldsSeq(phrase) // Split the phrase into words using FieldsSeq to handle multiple spaces.
-
-	for word := range words {
-		go func() {
-			count := countDigits(word)
-			countChan <- count
-		}()
-	}
-
-	stats := counter{}
-
-	for word := range words {
-		count := <-countChan
-		stats[word] = count
-	}
-
-	return stats
-}
-
-// asStats converts statistics from sync.Map to a regular map.
-func asStats(m *sync.Map) counter {
-	stats := counter{}
-	m.Range(func(word, count any) bool {
-		stats[word.(string)] = count.(int)
-		return true
-	})
-	return stats
-}
-
-// printStats prints the number of digits in words.
-func printStats(stats counter) {
-	for word, count := range stats {
-		fmt.Printf("%s: %d\n", word, count)
-	}
-}
 
 // Running both count functions as goroutines.
 func main() {
