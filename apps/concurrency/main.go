16a60b275a2023cd8b9d6bf33e8c9191879877ca
ndinhbang <ndinhbang111@gmail.com>
2026-03-25 08:35:20 +0700

    refactor(concurrency): Extract channel operations into separate functions for clarity

--- apps/concurrency/main.go
+++ apps/concurrency/main.go
@@ -5,25 +5,28 @@ import (
 	"strings"
 )
 
+// only send to the channel
+func submit(str string, stream chan<- string) {
+	words := strings.Split(str, ",")
+	for _, word := range words {
+		stream <- word
+	}
+	close(stream)
+}
+
+// only receive from the channel
+func receive(stream <-chan string) {
+	for word := range stream {
+		fmt.Printf("%s ", word)
+	}
+	fmt.Println()
+}
+
 // Running both count functions as goroutines.
 func main() {
 	str := "one,two,,four"
 
-	in := make(chan string)
-	go func() { // (1)
-		words := strings.Split(str, ",")
-		for _, word := range words {
-			in <- word
-		}
-		// close the channel to signal that there are no more words
-		// only one goroutine should close the channel
-		// if multiple goroutines close the channel, it will panic
-		// so we need to make sure that only one goroutine closes the channel
-		// and the other goroutines should not close the channel
-		close(in)
-	}()
-
-	for word := range in {
-		fmt.Printf("%s ", word)
-	}
+	stream := make(chan string)
+	go submit(str, stream)
+	receive(stream)
 }
